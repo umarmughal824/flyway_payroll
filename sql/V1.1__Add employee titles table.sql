20dcb379eccaf9675e9c093b0d7ce603b638d756
Umar Asghar <umarmughal824@yahoo.com_>
2017-07-13 15:21:06 +0500

    Added an employee titles table with initial employee titles data.

---
+++ sql/V1.1__Add employee titles table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE titles(
+ id INT NOT NULL AUTO_INCREMENT,
+ title VARCHAR(100) NOT NULL,
+ PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+INSERT INTO titles (title) VALUES ('Underling');
+INSERT INTO titles (title) VALUES ('Evil Overlord');
+INSERT INTO titles (title) VALUES ('Benevolent Dictator');
